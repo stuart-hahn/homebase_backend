3d6f718a655b3b428b2ea05476955d9e72c8afbf
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-06 17:05:18 -0400

    feat: generate tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  # GET /tasks
+  # This action fetches and returns all tasks from the database.
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  # GET /tasks/:id
+  # This action fetches a single task by its ID.
+  def show
+    @task = Task.find(params[:id])
+    render json: @task
+  end
+
+  # POST /tasks
+  # This action creates a new task in the database.
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/:id
+  # This action updates an existing task by its ID.
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/:id
+  # This action deletes a task by its ID.
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  # Strong parameters: allows only the title attribute to be passed in task creation and updates.
+  def task_params
+    params.require(:task).permit(:title)
+  end
+end

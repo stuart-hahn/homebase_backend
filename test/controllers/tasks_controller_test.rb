136615aefc43f62d820e739b5045dc9b141ad56c
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-06 17:07:20 -0400

    feat(tasks): add CRUD actions and controller tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,40 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test the index action (GET /tasks)
+  test "should get index" do
+    get tasks_url
+    assert_response :success
+  end
+
+  # Test the show action (GET /tasks/:id)
+  test "should show task" do
+    task = tasks(:one) # Assuming you have a fixture for Task
+    get task_url(task)
+    assert_response :success
+  end
+
+  # Test the create action (POST /tasks)
+  test "should create task" do
+    assert_difference('Task.count') do
+      post tasks_url, params: { task: { title: 'New Task' } }
+    end
+    assert_response :created
+  end
+
+  # Test the update action (PATCH/PUT /tasks/:id)
+  test "should update task" do
+    task = tasks(:one)
+    patch task_url(task), params: { task: { title: 'Updated Task' } }
+    assert_response :success
+  end
+
+  # Test the destroy action (DELETE /tasks/:id)
+  test "should destroy task" do
+    task = tasks(:one)
+    assert_difference('Task.count', -1) do
+      delete task_url(task)
+    end
+    assert_response :no_content
+  end
 end
